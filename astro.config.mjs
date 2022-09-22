9616b96baaaa970a60728365c86bd8224429f8dd
albertevieites <albertevieites@gmail.com>
2022-09-22 19:07:46 +0100

    feat: :sparkles: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.albertevieites.com',
-  integrations: [image(), react()]
+  integrations: [image(), react(), sitemap()]
 });
