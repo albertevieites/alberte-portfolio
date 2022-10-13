55e856070dc19dcef583715f277f78368e822675
albertevieites <albertevieites@gmail.com>
2022-10-13 22:06:29 +0100

    add SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
     image(),
     react(),
     sitemap(),
+    robotsTxt(),
   ]
 });
