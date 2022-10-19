37e323853d072ce3965ee082f8a3f9f81a56a94b
albertevieites <albertevieites@gmail.com>
2022-10-19 22:16:15 +0100

    spliteType animation

---
+++ src/scripts/splitType.js
@@ -0,0 +1,10 @@
+const myText1 = new SplitType('#target1')
+const myText2 = new SplitType('#target2')
+const myText3 = new SplitType('#target3')
+
+gsap.to('.char', {
+  y: 0,
+  stagger: 0.05,
+  delay: 0.2,
+  duration: .1,
+})
