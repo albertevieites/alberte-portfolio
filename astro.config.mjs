3192f5bf529a0fa49c4dacf06eb2c53322656aa0
albertevieites <albertevieites@gmail.com>
2022-10-24 20:31:18 +0100

    feat: :lipstick: add tag borders

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+// Astro integrations imports
+import vercel from "@astrojs/vercel/serverless";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
+	adapter: vercel(),
   site: 'https://www.albertevieites.com',
   integrations: [
     image(),
