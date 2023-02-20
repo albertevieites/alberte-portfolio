5f440a93f2a1736fffc6db5c5b62a6e51f53c765
Alberte Vieites <albertevieites@gmail.com>
2023-02-20 12:03:05 +0000

    feat: :sparkles: add gocha project

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,12 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercel(),
+  output: 'server',
+  adapter: vercel(),
   site: 'https://www.albertevieites.com',
-  integrations: [
-    image(),
-    sitemap(),
-    robotsTxt(),
-  ]
+  integrations: [image(), sitemap(), robotsTxt(), react()]
 });
