b062fe40bad981c93d5b4efeb8bcc4f04476bc1e
Alberte Vieites <albertevieites@gmail.com>
2023-12-12 21:43:57 +0000

    Upgrade whole structure to Astro 4.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // Astro integrations imports
 import vercel from "@astrojs/vercel/serverless";
-import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
@@ -14,5 +13,5 @@ export default defineConfig({
   output: 'server',
   adapter: vercel(),
   site: 'https://www.albertevieites.com',
-  integrations: [image(), sitemap(), robotsTxt(), react()]
+  integrations: [sitemap(), robotsTxt(), react()]
 });
